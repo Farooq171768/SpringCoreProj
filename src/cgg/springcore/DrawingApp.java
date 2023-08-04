a4e77634214dda80080eff91c2da9173a3a9b0be
Farooq <fmd894717@gmail.com>
2023-08-04 17:10:03 +0530

    commit 04-08-2023

--- src/cgg/springcore/DrawingApp.java
+++ src/cgg/springcore/DrawingApp.java
@@ -1,18 +1,24 @@
 package cgg.springcore;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		//Triangle triangle = new Triangle();
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		//Dependency Injection
-		Triangle triangle =(Triangle) factory.getBean("triangle");
+		//Triangle triangle = new Triangle(); - we can avoid this new method
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+			//If there is any constrains on resource, go with BeanFactory
+		
+		//ApplicationContext is big brother of BeanFactory
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		//Triangle triangle =(Triangle) factory.getBean("triangle");
+		
+		Triangle triangle =(Triangle) context.getBean("triangle");
 		
 		triangle.draw();
 	}
-
 }
--- src/cgg/springcore/Triangle.java
+++ src/cgg/springcore/Triangle.java
@@ -1,8 +1,86 @@
 package cgg.springcore;
 
+import java.util.List;
+
 public class Triangle {
 
-	public void draw() {
-		System.out.println("Triangle drawn");
+	/*
+	 * private String type; private int height;
+	 * 
+	 * //One arg Constructor public Triangle(String type) { super(); this.type =
+	 * type; } //Another one arg constructor public Triangle(int height) { super();
+	 * this.height= height; }
+	 * 
+	 * //Two arg constructor public Triangle(String type, int height) { super();
+	 * this.type = type; this.height = height; }
+	 * 
+	 * public String getType() { return type; }
+	 * 
+	 * // public void setType(String type) { // this.type = type; // }
+	 * 
+	 * 
+	 * 
+	 * public int getHeight() { return height; }
+	 * 
+	 * public void setHeight(int height) { this.height = height; }
+	 */
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+	
+	/*
+	 * private List<Point> points;
+	 * 
+	 * public List<Point> getPoints() { return points; }
+	 * 
+	 * public void setPoints(List<Point> points) { this.points = points; }
+	 */
+
+	
+
+	public void draw() {
+		//System.out.println(getType() + " Triangle drawn of height " + getHeight());
+		System.out.println("Point A =("+getPointA().getX()+", "+getPointA().getY()+")");
+		System.out.println("Point B =("+getPointB().getX()+", "+getPointB().getY()+")");
+		System.out.println("Point C =("+getPointC().getX()+", "+getPointC().getY()+")");
+		
+//		for (Point point : points) {
+//			System.out.println("Point = ("+point.getX()+", "+point.getY()+")");
+//		}
+		
+		
+	}
+
+
+
+
+	
+
+
+
+
 }
